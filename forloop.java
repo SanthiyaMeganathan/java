aa061194c532bad0ec2f18de901dc7b4f155d9aa
SanthiyaMeganathan <120907788+SanthiyaMeganathan@users.noreply.github.com>
2024-08-19 18:33:53 +0530

    msg

---
+++ forloop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class forloop {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+            System.out.println("helloworld");
+
+            
+            
+        }
+    }
+    
+}
