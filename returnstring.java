0d147d004a39c278d6d27e09debf9b102dbebec3
SanthiyaMeganathan <120907788+SanthiyaMeganathan@users.noreply.github.com>
2024-09-01 13:38:52 +0530

    funtion retrun string

---
+++ returnstring.java
@@ -0,0 +1,14 @@
+
+
+public class returnstring {
+
+    public static void main(String[] args) {
+        String str = greeting();
+        System.out.println(str);
+        
+    }
+
+    static String greeting() {
+        return "Hello, World!";
+    }
+}
